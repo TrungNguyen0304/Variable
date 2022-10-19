6f950d4076da6d8142963e5c6a61fec793b4df3f
TrungNguyen0304 <113655575+TrungNguyen0304@users.noreply.github.com>
2022-10-19 20:49:46 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,28 @@
+package basic.dev;
+/**
+ * 
+ * @author NguyenDinhTrung
+ *
+ */
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	  /**
+	   * trung123132
+	   */
+		String firstName = "Trung Nguyen";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println("Nguyen Dinh Trung");
+		System.out.println("Trung Nguyen +  Nam");
+		System.out.println("Mark Math:" + 9f );
+		System.out.println("Passed: "+ true );
+		final float PI = 3.14f;	
+	}
+
+}
